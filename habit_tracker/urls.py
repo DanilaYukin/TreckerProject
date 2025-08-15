39733491bf55ac96dd437b172eb592abe99dcd8f
Daniil <danilayukin@gmail.com>
2025-08-15 23:25:31 +0300

    Done

---
+++ habit_tracker/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from habit_tracker.apps import HabitTrackerConfig
+from habit_tracker.views import (
+    HabitCreateApiView,
+    HabitUpdateApiView,
+    HabitDestroyApiView,
+    HabitRetrieveApiView,
+    HabitUserListApiView,
+    HabitIsPublicListApiView,
+)
+
+app_name = HabitTrackerConfig.name
+
+urlpatterns = [
+    path("habit/create/", HabitCreateApiView.as_view(), name="habit_create"),
+    path("habits_user/", HabitUserListApiView.as_view(), name="habits_user_list"),
+    path("habits_public/", HabitIsPublicListApiView.as_view(), name="habits_user_list"),
+    path("habit/<int:pk>/", HabitRetrieveApiView.as_view(), name="habit_get"),
+    path("habit/update/<int:pk>/", HabitUpdateApiView.as_view(), name="habit_update"),
+    path("habit/delete/<int:pk>/", HabitDestroyApiView.as_view(), name="habit_delete"),
+]
